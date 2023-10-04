578128ff48392387b37ca6f02ab46c495e7bbbc6
hxclover <rbecca.lam@gmail.com>
2023-10-03 17:10:02 -0700

    working converter

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+   return ((fahren - 32) * (5 / 9));
+}
+
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +25,30 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  let msg = '';
+  
+  if(fahren <= 32) {
+    msg = 'Very cold.';
+  } else if(fahren <= 64) {
+    msg = 'Cold.';
+  } else if(fahren < 86) {
+    msg = 'Warm.';
+  } else  {
+    msg = 'Very hot.';
+  }
+  return `${fahren}F is ${celc}C. ${msg}`
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.round(Math.random()*limit);
+}
+
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
